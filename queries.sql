4a537066ec77a081983fb2852fe0946d5b99ee8f
Ayomide <ayomidesunday31@gmail.com>
2023-08-25 23:46:01 +0100

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -22,4 +22,42 @@ SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
-SELECT * FROM animals WHERE weight_kg BETWEEN '10.4' AND '17.3'; 
+SELECT * FROM animals WHERE weight_kg BETWEEN '10.4' AND '17.3';
+
+-- DAY - 2
+
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction.
+SAVEPOINT sp1;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO sp1;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg *-1 WHERE weight_kg < 1;
+
+COMMIT;
+
+-- How many animals are there?
+ SELECT COUNT(name) FROM animals;
+
+--  How many animals have never tried to escape?
+SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, COUNT(*) FROM animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT neutered, MAX(weight_kg) FROM animals GROUP BY neutered;
+SELECT neutered, MIN(weight_kg) FROM animals GROUP BY neutered;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT neutered, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN DATE '1990-01-01' AND '2000-01-01' GROUP BY neutered;
